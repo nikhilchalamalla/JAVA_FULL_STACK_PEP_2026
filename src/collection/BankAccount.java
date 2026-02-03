e7cd9444ed855eae7d940149794e57995af68000
Chalamalla Nikhil <nikhilchalamalla4@gmail.com>
2026-02-03 21:49:04 +0530

    Added BankAccount

---
+++ src/collection/BankAccount.java
@@ -0,0 +1,35 @@
+package collection;
+
+public class BankAccount {
+    int accountNo;
+    String name;
+    double balance;
+
+    BankAccount(int accountNo, String name, double balance) {
+        this.accountNo = accountNo;
+        this.name = name;
+        this.balance = balance;
+    }
+
+    void deposit(double amount) {
+        balance += amount;
+    }
+
+    void withdraw(double amount) {
+        if (amount > balance)
+            System.out.println("Insufficient Balance");
+        else
+            balance -= amount;
+    }
+
+    void checkBalance() {
+        System.out.println("Balance: " + balance);
+    }
+
+    public static void main(String[] args) {
+        BankAccount acc = new BankAccount(101, "Nikhil", 5000);
+        acc.withdraw(6000);
+        acc.deposit(2000);
+        acc.checkBalance();
+    }
+}
