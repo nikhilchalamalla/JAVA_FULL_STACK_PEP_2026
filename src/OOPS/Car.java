b610bc2cfa4e3c238d045f7b1c2f8fab276ff5d0
Chalamalla Nikhil <nikhilchalamalla4@gmail.com>
2026-02-07 21:43:28 +0530

    Added Car

---
+++ src/OOPS/Car.java
@@ -0,0 +1,20 @@
+package OOPS;
+
+public class Car {
+    String model;
+    int pricePerDay;
+
+    Car(String model, int pricePerDay) {
+        this.model = model;
+        this.pricePerDay = pricePerDay;
+    }
+
+    int calculateRent(int days) {
+        return days * pricePerDay;
+    }
+
+    public static void main(String[] args) {
+        Car car = new Car("Honda", 1500);
+        System.out.println(car.calculateRent(3));
+    }
+}
