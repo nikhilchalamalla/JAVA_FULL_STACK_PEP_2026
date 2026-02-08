ddbf050c099981fb21a567547ea64f91c15ab882
Chalamalla Nikhil <nikhilchalamalla4@gmail.com>
2026-02-08 22:15:24 +0530

    Added MinMaxList

---
+++ src/Collections/MinMaxList.java
@@ -0,0 +1,9 @@
+package Collections;
+import java.util.*;
+public class MinMaxList {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(10,5,30,2);
+        System.out.println(Collections.max(list));
+        System.out.println(Collections.min(list));
+    }
+}
